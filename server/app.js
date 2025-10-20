8384717f9133b0f13d769071b04336382851ff3d
joshzru <142758778+joshzru@users.noreply.github.com>
2025-10-20 14:50:20 +0000

    server setup to serve static files from public/geojson/ using compression

--- server/app.js
+++ server/app.js
@@ -2,19 +2,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compress = require('compression');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(compress());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve all static files in public/geojson/
+app.use('/geojson', express.static(path.join(__dirname, 'public', 'geojson')));
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
