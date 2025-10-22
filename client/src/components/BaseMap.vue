1124bab5642f4c11d63db6eef54a8d1d79fb3fd9
joshzru <142758778+joshzru@users.noreply.github.com>
2025-10-22 15:55:39 +0000

    Added filter box for map

--- client/src/components/BaseMap.vue
+++ client/src/components/BaseMap.vue
@@ -25,7 +25,14 @@ export default {
 
 <template>
     <div class="container">
-        <svg ref="svg" width="1600" height="800" viewBox="0 0 1600 800"></svg>
+        <svg ref="svg" width="1200" height="800" viewBox="0 0 1600 800"></svg>
+        <fieldset class="checkboxes">
+            <legend>Filters:</legend>
+            <label><input type="checkbox"> Filter 1</label>
+            <label><input type="checkbox"> Filter 2</label>
+            <label><input type="checkbox"> Filter 3</label>
+            <label><input type="checkbox"> Filter 4</label>
+        </fieldset>
         <RailroadData :svgElement="svg" :projection="projection" />
         <CityData :svgElement="svg" :projection="projection" />
         <BorderData :svgElement="svg" :projection="projection" />
@@ -36,5 +43,15 @@ export default {
 .container {
     display: flex;
     flex-direction: row;
+    align-items: center;
+}
+
+.checkboxes {
+    display: flex;
+    flex-direction: column;
+}
+
+.checkboxes label {
+    padding: 5px 0px;
 }
 </style>
