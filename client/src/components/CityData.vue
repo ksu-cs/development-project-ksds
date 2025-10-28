ab4168f34761e3c3d819c8e69ab2011e79bf6f13
joshzru <joshzru@gmail.com>
2025-10-28 19:27:04 +0000

    Refactored data components to wait until the svg becomes available to render

--- client/src/components/CityData.vue
+++ client/src/components/CityData.vue
@@ -10,14 +10,12 @@ import * as d3 from 'd3';
 
 export default {
     props: ["svgElement", "projection"],
-    async mounted() {
-        const response = await fetch('/geojson/cities.geojson');
-        const data = await response.json();
+    methods: {
+        renderData(data) {
+            const pathGen = d3.geoPath(this.projection).pointRadius(1.5)
 
-        const pathGen = d3.geoPath(this.projection).pointRadius(1.5)
-
-        // Create path elements for city lon and lat coordinates
-        d3.select(this.svgElement)
+            // Create path elements for city lon and lat coordinates
+            d3.select(this.svgElement.value)
             .append("g")
                 .classed("cities", true)
             .append("g")
@@ -29,28 +27,49 @@ export default {
                 .attr("d", pathGen)
                 .classed("city", true);
 
-        // Project every city's lon and lat coordinates
-        // pathGen does this already in the selection above
-        // however, the next selection doesn't use pathGen
-        data.features = data.features.map(feature => {
-            feature.geometry.coordinates = this.projection(feature.geometry.coordinates);
-            return feature;
-        })
+            // Project every city's lon and lat coordinates
+            // pathGen does this already in the selection above
+            // however, the next selection doesn't use pathGen
+            data.features = data.features.map(feature => {
+                feature.geometry.coordinates = this.projection(feature.geometry.coordinates);
+                return feature;
+            })
 
-        // Create text elements for city names
-        d3.select(this.svgElement)
-            .select("g.cities")
-            .append("g")
-                .classed("city-names", true)
-            .selectAll(".city-name")
-            .data(data.features)
-            .enter()
-            .filter(d => d.properties["Top Ten"])
-            .append("text")
-                .attr("x", d => d.geometry.coordinates[0])
-                .attr("y", d => d.geometry.coordinates[1] - 2)
-                .property("textContent", d => d.properties["City Name"])
-                .classed("city-name", true);
+            // Create text elements for city names
+            d3.select(this.svgElement.value)
+                .select("g.cities")
+                .append("g")
+                    .classed("city-names", true)
+                .selectAll(".city-name")
+                .data(data.features)
+                .enter()
+                .filter(d => d.properties["Top Ten"])
+                .append("text")
+                    .attr("x", d => d.geometry.coordinates[0])
+                    .attr("y", d => d.geometry.coordinates[1] - 2)
+                    .property("textContent", d => d.properties["City Name"])
+                    .classed("city-name", true);
+        }
+    },
+    created() {
+        // fetch the geojson and then watch for the svg element
+        // to become mounted, unless it's already mounted
+        fetch('/geojson/cities.geojson')
+        .then(response => response.json())
+        .then(geojson => {
+            if (this.svgElement.value) {
+                this.renderData(geojson);
+            }
+            else {
+                const unwatch = this.$watch(() => this.svgElement.value,
+                                            newValue => {
+                                                if (newValue) {
+                                                    this.renderData(geojson);
+                                                    unwatch();
+                                                }
+                                            })
+            }
+        })
     }
 }
 </script>
