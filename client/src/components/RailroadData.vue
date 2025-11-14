f0c9a7511fe666afeb14ba8f1e834b9d5839e069
joshzru <142758778+joshzru@users.noreply.github.com>
2025-11-14 00:38:29 +0000

    Re-refactored RailroadData.vue to use the <script setup> tag

--- client/src/components/RailroadData.vue
+++ client/src/components/RailroadData.vue
@@ -1,71 +1,83 @@
-<script>
+<script setup>
+import { defineProps, onMounted, useTemplateRef, watch } from 'vue';
 import * as d3 from 'd3';
-import { fetchGeojson } from './fetchGeojson';
+import { fetchGeojson } from './fetchGeojson'
 
-export default {
-    props: [ "projection", "inputValue", "zoomState"],
-    data() {
-        return {
-            pathGen: d3.geoPath(this.projection),
-            selection: null,
-            gTag: null
-        }
-    },
-    methods: {
-        validateData() {
-            if (this.loading) {
-                const unwatch = this.$watch(() => this.loading, () => { this.validateData(); unwatch() });
-            } else if (this.error) {
-                console.log(this.error);
-            } else {
-                console.log(this.data);
-                this.selection = this.gTag.selectAll("path")
-                                            .data(this.data.features)
-                                            .enter()
-                                            .append("path");
-                
-                this.selection
-                        .attr("d", this.pathGen)
-                        .attr("stroke-opacity", d => d.properties.InOpBy <= this.inputValue ? "60%" : "0%")
-                        .attr("stroke-width", 1)
-                        .classed("rail", true);
-            }
-        },
-        onZoom(newValue) {
-            if (newValue === "state") {
-                this.selection
-                    .transition()
-                    .duration(200)
-                        .attr("stroke-width", 0.6);
-            } else if (newValue == "county") {
-                this.selection
-                    .transition()
-                    .duration(200)
-                        .attr("stroke-width", 1);
-            }
-        },
-        onYearChange(newValue) {
-            this.selection
-                .transition()
-                .duration(200)
-                    .attr("stroke-opacity", d => d.properties.InOpBy <= newValue ? "60%" : "0%");
-        }
-    },
-    setup() {
-        // Fetch and validate data.
-        const pathString = 'railroads.geojson';
-        const { data, loading, error } = fetchGeojson(pathString);
+const props = defineProps(["projection", "inputValue", "zoomState"]);
+const { data, loading, error } = fetchGeojson("railroads.geojson");
+const pathGen = d3.geoPath(props.projection);
+const gRef = useTemplateRef("g");
 
-        // Expose properties.
-        return { pathString, data, loading, error }
-    },
-    mounted() {
-        this.gTag = d3.select(this.$refs.g);
-        console.log(this.gTag);
-        this.validateData();
-        this.$watch(() => this.zoomState, this.onZoom);
-        this.$watch(() => this.inputValue, this.onYearChange);
+let selection = null;
+let gTag = null;
+
+onMounted(() => {
+    gTag = d3.select(gRef.value);
+    validateData();
+});
+
+watch(() => props.zoomState.value, onZoom);
+watch(() => props.inputValue.value, onYearChange);
+
+/**
+ * Waits for the fetched data to load. If the fetch failed,
+ * prints the error recieved. If the fetch succeeded, binds
+ * the data to the selection.
+ */
+function validateData() {
+    if (loading.value) {
+        // Watch for the data to load
+        const unwatch = watch(() => loading.value, () => { validateData(); unwatch() });
+    } else if (error.value) {
+        console.log(error.value);
+    } else {
+        // Create rail path elements
+        selection = gTag.selectAll("path")
+                                    .data(data.value.features)
+                                    .enter()
+                                    .append("path");
+        
+        // Style rail path elements
+        selection.attr("d", pathGen)
+                .attr("stroke-opacity", d => d.properties.InOpBy <= props.inputValue ? "60%" : "0%")
+                .attr("stroke-width", 1)
+                .classed("rail", true);
     }
+}
+
+/**
+ * Changes width of rail path elements based on the
+ * zoomState.
+ * @param newValue The new zoomState string
+ */
+function onZoom(newValue) {
+    switch (newValue) {
+        case "state":
+            selection
+                .transition()
+                    .duration(200)
+                    .attr("stroke-width", 0.6);
+            break;
+        case "county":
+            selection
+                .transition()
+                    .duration(200)
+                    .attr("stroke-width", 1);
+            break;
+    }
+}
+
+/**
+ * Fades in all rail path elements that were in operation
+ * by the given year. Fades out those elements that were not
+ * in operation by the given year.
+ * @param newValue The year selected
+ */
+function onYearChange(newValue) {
+    selection
+        .transition()
+            .duration(200)
+            .attr("stroke-opacity", d => d.properties.InOpBy <= newValue ? "60%" : "0%");
 }
 </script>
 
