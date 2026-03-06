e9c3b18e57a0602995545caa9b5f715612084b76
Evan Harris <142772669+EvanWHarris@users.noreply.github.com>
2026-03-06 17:56:20 +0000

    Completed water bodies

---
+++ client/src/components/LakeData.vue
@@ -0,0 +1,82 @@
+<script setup>
+	/**
+	 * components/LakeData.vue
+	 * Responsible for rendering lake polygons.
+	 */
+	import {defineProps, onMounted, useTemplateRef, watch, inject} from 'vue';
+	import * as d3 from 'd3';
+	import {fetchGeojson} from './fetchers';
+	import {fadeOut, fadeIn} from '@/d3/transitions/fadeSelection';
+	import {registerKey} from './RegisterKey';
+	import {MapZoomLevel} from '@/enums/MapZoomLevel';
+	import {GroupType} from '@/enums/GroupType';
+
+	const props = defineProps(['properties']);
+	const gRef = useTemplateRef('g');
+
+	const pathGen = d3.geoPath(props.properties.projection);
+	const label = 'lakes';
+
+	// Register with BaseMap for the filter UI
+	inject(registerKey)(label, {
+		filter: {
+			legibleLabel: 'Lakes',
+			defaultStatus: true,
+			visibleStates: new Set([MapZoomLevel.STATE, MapZoomLevel.COUNTY]),
+			groups: [GroupType.OTHER],
+			onChecked: () => fadeIn(selection),
+			onUnchecked: () => fadeOut(selection),
+		},
+	});
+
+	let selection = null;
+	let gTag = null;
+	let paths = {
+		geojson: `${props.properties.path}/geojson`,
+	};
+
+	onMounted(() => {
+		gTag = d3.select(gRef.value);
+		const {result} = fetchGeojson(`${paths.geojson}/KS_Lakes.geojson`);
+		renderToSVG(result);
+	});
+
+	function renderToSVG(r) {
+		const d = r.data.value;
+		const l = r.loading.value;
+
+		if (l) {
+			const unwatch = watch(
+				() => r.loading.value,
+				() => {
+					renderToSVG(r);
+					unwatch();
+				}
+			);
+			return;
+		}
+
+		selection = gTag
+			.selectAll('.lake')
+			.data(d.features)
+			.join('path')
+			.attr('d', (f) => {
+				// Polygons often need winding order reversal for D3
+				if (f.geometry.coordinates[0]) f.geometry.coordinates[0].reverse();
+				return pathGen(f);
+			})
+			.classed('lake', true);
+	}
+</script>
+
+<template>
+	<g class="lakes" ref="g"></g>
+</template>
+
+<style scoped>
+	:global(.lake) {
+		fill: #add8e6;
+		stroke: #5bc0de;
+		stroke-width: 0.2;
+	}
+</style>
